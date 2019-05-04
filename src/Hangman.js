7cd26880b0c30cb992a6248d17f2329efd7a16f2
xaviguasch <xaviguasch@gmail.com>
2019-05-04 11:01:38 +0200

    dynamically added alt to each photo

--- src/Hangman.js
+++ src/Hangman.js
@@ -51,12 +51,12 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
-    let gameOver = this.state.nWrong >= this.props.maxWrong
-
+    const gameOver = this.state.nWrong >= this.props.maxWrong
+    const altText = `${this.state.nWrong}/${this.props.maxWrong}`
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
         <p>Guessed Wrong: {this.state.nWrong}</p>
         <p className='Hangman-word'>{!gameOver ? this.guessedWord() : this.state.answer}</p>
         <p className='Hangman-btns'>{!gameOver ? this.generateButtons() : 'You lose!'}</p>
